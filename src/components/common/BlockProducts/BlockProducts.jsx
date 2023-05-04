c76031f2647c7801dc6100e644ac1695cd1e2088
NubloEg <106815766+NubloEg@users.noreply.github.com>
2023-05-04 20:34:03 +0300

    Revert "Dev-Egor commit"
    This reverts commit 3d8efffd

--- src/components/common/BlockProducts/BlockProducts.jsx
+++ src/components/common/BlockProducts/BlockProducts.jsx
@@ -3,13 +3,6 @@ import style from "./BlockProduct.module.css"
 import BlockProductsCategory from '../UI/BlockProductsCategory'
 import BlockProductsCard from './BlockProductsCard/BlockProductsCard'
 
-import {Swiper,SwiperSlide} from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/effect-coverflow'
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
-
-import { EffectCoverflow,Pagination,Navigation } from 'swiper'
  function BlockProducts({title,category,items}) {
   return (
     <div className={style.blockproducts}>
@@ -19,23 +12,12 @@ import { EffectCoverflow,Pagination,Navigation } from 'swiper'
         {category.map((el,id)=><BlockProductsCategory key={id}>{el}</BlockProductsCategory>)}
         </div>
         <div className={style.blockproducts__cards}>
-            <div className={`${style.blockproducts__cards_left} left`}></div>
-            <Swiper
-            effect='coverflow'
-            grabCursor={true}
-            centeredSlides={true}
-            loop={true}
-            slidesPerView='auto'
-
-            navigation={{
-              nextEl:'right',
-              prevEl:'left'
-            }}
-            className={style.blockproducts__cards__items} >
-            {items.map((el,id)=><SwiperSlide><BlockProductsCard /></SwiperSlide>)}
-            </Swiper>
+            <div className={style.blockproducts__cards_left}></div>
+            <div className={style.blockproducts__cards__items} >
+            {items.map((el,id)=><BlockProductsCard />)}
+            </div>
            
-            <div className={`${style.blockproducts__cards_right} right`}></div>
+            <div className={style.blockproducts__cards_right}></div>
         </div>
         </div>
        
