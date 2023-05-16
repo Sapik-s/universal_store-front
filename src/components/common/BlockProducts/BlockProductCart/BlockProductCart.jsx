3588bc428801d0e2270d5ee999b8c1f5dacf8a77
NubloEg <106815766+NubloEg@users.noreply.github.com>
2023-05-16 08:06:16 +0300

    change

--- src/components/common/BlockProducts/BlockProductCart/BlockProductCart.jsx
+++ src/components/common/BlockProducts/BlockProductCart/BlockProductCart.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import style from "./BlockProductCart.module.css"
+import Like from "../UI/Like";
+import Cart from "../UI/Cart";
+import {Link} from "react-router-dom";
 
-/* import like from "../UI/like.svg"
-import cart from "../UI/cart.svg" */
+
 
  function BlockProductsCard({id,title,category,margin}) {
+     let count=1
   return (
     <div className={style.cart_container} style={id===0?{marginLeft:margin}:{}}  id={id}>
-      <div className={style.card_item}>
+      <Link to={'/items'} className={ `${style.card_item} ${style.card_item_watch}`}>
+
       <div className={style.card_item__sale}>SALE</div>
-       <img alt='like' /* src={like} */ className={style.card_item__like}/>
+       <Like className={style.card_item__like}/>
       <div className={style.card_item__image}></div>
       <div className={style.card_item__title}>Водонепроницаемый
 Рюкзак {id}</div>
-      <div className={style.card_item__price}>9 800 ₽</div>
-      <img alt='cart'/*  src={cart} */ className={style.card_item__cart}/>
-    </div>
+          {count!==0? <div className={style.card_item__price}>9 800 ₽</div>
+              :
+              <div className={style.card_item_none}>
+              <div >Нет в наличии</div>
+              <div className={style.card_item_none_link} >Сообщить о поступлении</div>
+          </div>}
+          <Cart  className={style.card_item__cart}/>
+    </Link>
     </div>
   )
 }
